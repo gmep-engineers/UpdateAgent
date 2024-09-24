1cb446054fc95c9fa58439caf0ea46d39cce514e
grenrock <stevengrenrock@live.com>
2024-09-24 16:17:50 -0700

    remove GMEPElectricalCommands

--- Installer.ps1
+++ Installer.ps1
@@ -1,5 +1,5 @@
 $localDestDir = "$Env:userprofile\AutoCAD Commands"
 Copy-Item .\startup.cmd -Destination "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Startup"
-mkdir -Force -Path "$localDestDir\GMEPElectricalCommands"
+mkdir -Force -Path "$localDestDir"
 Copy-Item .\UpdateAgent.ps1 -Destination "$localDestDir"
 & $localDestDir\UpdateAgent.ps1
