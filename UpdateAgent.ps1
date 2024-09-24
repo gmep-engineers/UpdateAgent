5bde3f33e47d7e415b3fe330b1fb72d0aaa4539e
grenrock <stevengrenrock@live.com>
2024-09-24 15:39:01 -0700

    define zDriveSourceDir and $localDestDir

--- UpdateAgent.ps1
+++ UpdateAgent.ps1
@@ -1,18 +1,20 @@
 # 2024-09-23: This currently only covers GMEPElectricalCommands.
 # Expand this file to cover the other command suites when they are ready to be integrated into the UpdateAgent.
+$zDriveSourceDir = "Z:\GMEP Engineers\Users\GMEP Softwares\AutoCAD Commands"
+$localDestDir = "$Env:userprofile\AutoCAD Commands"
 try {
-    $zDriveDll = Get-Item "Z:\GMEP Engineers\Users\GMEP Softwares\AutoCAD Commands\GMEPElectricalCommands\latest\ElectricalCommands.dll" -ErrorAction Stop
-    $localDll = Get-Item "$Env:userprofile\AutoCAD Commands\GMEPElectricalCommands\ElectricalCommands.dll" -ErrorAction Stop
+    $zDriveDll = Get-Item "$zDriveSourceDir\GMEPElectricalCommands\latest\ElectricalCommands.dll" -ErrorAction Stop
+    $localDll = Get-Item "$localDestDir\GMEPElectricalCommands\ElectricalCommands.dll" -ErrorAction Stop
     if ($zDriveDll.CreationTime -gt $localDll.CreationTime) {
-        Copy-Item -Path "Z:\GMEP Engineers\Users\GMEP Softwares\AutoCAD Commands\GMEPElectricalCommands\latest\*" -Destination "$Env:userprofile\AutoCAD Commands\GMEPElectricalCommands" -Recurse
-        Copy-Item -Path "Z:\GMEP Engineers\Users\GMEP Softwares\AutoCAD Commands\load_dll.lsp" -Destination "$Env:userprofile\AutoCAD Commands"
-        Get-ChildItem -Recurse "$Env:userprofile\AutoCAD Commands\GMEPElectricalCommands\" | Unblock-File
-        (Get-Content "$Env:userprofile\AutoCAD Commands\load_dll.lsp") -replace "%USERPROFILE%", "$Env:userprofile" -replace "\\", "/" | Out-File "$Env:userprofile\AutoCAD Commands\load_dll.lsp"  
+        Copy-Item -Path "$zDriveSourceDir\GMEPElectricalCommands\latest\*" -Destination "$localDestDir\GMEPElectricalCommands" -Recurse
+        Copy-Item -Path "$zDriveSourceDir\load_dll.lsp" -Destination "$localDestDir"
+        Get-ChildItem -Recurse "$localDestDir\GMEPElectricalCommands\" | Unblock-File
+        (Get-Content "$localDestDir\load_dll.lsp") -replace "%USERPROFILE%", "$Env:userprofile" -replace "\\", "/" | Out-File "$localDestDir\load_dll.lsp"  
     }
 } catch {
-    mkdir -Force -Path "$Env:userprofile\AutoCAD Commands\GMEPElectricalCommands"
-    Copy-Item -Path "Z:\GMEP Engineers\Users\GMEP Softwares\AutoCAD Commands\GMEPElectricalCommands\latest\*" -Destination "$Env:userprofile\AutoCAD Commands\GMEPElectricalCommands" -Recurse
-    Copy-Item -Path "Z:\GMEP Engineers\Users\GMEP Softwares\AutoCAD Commands\load_dll.lsp" -Destination "$Env:userprofile\AutoCAD Commands"
-    Get-ChildItem -Recurse "$Env:userprofile\AutoCAD Commands\GMEPElectricalCommands\" | Unblock-File
-    (Get-Content "$Env:userprofile\AutoCAD Commands\load_dll.lsp") -replace "%USERPROFILE%", "$Env:userprofile" -replace "\\", "/" | Out-File "$Env:userprofile\AutoCAD Commands\load_dll.lsp"
+    mkdir -Force -Path "$localDestDir\GMEPElectricalCommands"
+    Copy-Item -Path "$zDriveSourceDir\GMEPElectricalCommands\latest\*" -Destination "$localDestDir\GMEPElectricalCommands" -Recurse
+    Copy-Item -Path "$zDriveSourceDir\load_dll.lsp" -Destination "$localDestDir"
+    Get-ChildItem -Recurse "$localDestDir\GMEPElectricalCommands\" | Unblock-File
+    (Get-Content "$localDestDir\load_dll.lsp") -replace "%USERPROFILE%", "$Env:userprofile" -replace "\\", "/" | Out-File "$localDestDir\load_dll.lsp"
 }
