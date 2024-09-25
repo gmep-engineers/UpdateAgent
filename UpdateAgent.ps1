2069fc1c1a6a41bed6cbf69e2f80046d4905177e
grenrock <stevengrenrock@live.com>
2024-09-25 10:19:24 -0700

    add repos array

--- UpdateAgent.ps1
+++ UpdateAgent.ps1
@@ -2,19 +2,37 @@
 # Expand this file to cover the other command suites when they are ready to be integrated into the UpdateAgent.
 $zDriveSourceDir = "Z:\GMEP Engineers\Users\GMEP Softwares\AutoCAD Commands"
 $localDestDir = "$Env:userprofile\AutoCAD Commands"
+$repos = @(
+    [pscustomobject]@{name="GMEPElectricalCommands";dll="ElectricalCommands.dll"},
+    [pscustomobject]@{name="GMEPElectricalResidential";dll="GMEPElectricalResidential.dll"}
+)
+foreach($repo in $repos) {
+    try {
+        $name = $repo.name
+        $dll = $repo.dll
+        $zDriveDll = Get-Item "$zDriveSourceDir\$name\latest\$dll" -ErrorAction Stop
+        $localDll = Get-Item "$localDestDir\$name\$dll" -ErrorAction Stop
+        if ($zDriveDll.CreationTime -gt $localDll.CreationTime) {
+            Copy-Item -Path "$zDriveSourceDir\$name\latest\*" -Destination "$localDestDir\$name" -Recurse
+            Get-ChildItem -Recurse "$localDestDir\$name" | Unblock-File
+        }
+    } catch {
+        mkdir -Force -Path "$localDestDir\$name"
+        Copy-Item -Path "$zDriveSourceDir\$name\latest\*" -Destination "$localDestDir\$name" -Recurse
+        Get-ChildItem -Recurse "$localDestDir\$name" | Unblock-File
+    }
+}
+
 try {
-    $zDriveDll = Get-Item "$zDriveSourceDir\GMEPElectricalCommands\latest\ElectricalCommands.dll" -ErrorAction Stop
-    $localDll = Get-Item "$localDestDir\GMEPElectricalCommands\ElectricalCommands.dll" -ErrorAction Stop
-    if ($zDriveDll.CreationTime -gt $localDll.CreationTime) {
-        Copy-Item -Path "$zDriveSourceDir\GMEPElectricalCommands\latest\*" -Destination "$localDestDir\GMEPElectricalCommands" -Recurse
+    $zDriveLsp = Get-Item "$zDriveSourceDir\load_dll.lsp" -ErrorAction Stop
+    $localLsp = Get-Item "$localDestDir\load_dll.lsp" -ErrorAction Stop
+    if ($zDriveLsp.CreationTime -gt $localLsp.CreationTime) {
         Copy-Item "$zDriveSourceDir\load_dll.lsp" -Destination "$localDestDir"
-        Get-ChildItem -Recurse "$localDestDir\GMEPElectricalCommands\" | Unblock-File
-        (Get-Content "$localDestDir\load_dll.lsp") -replace "%USERPROFILE%", "$Env:userprofile" -replace "\\", "/" | Out-File "$localDestDir\load_dll.lsp"  
+        Get-Item "$localDestDir\load_dll.lsp" | Unblock-File
+        (Get-Content "$localDestDir\load_dll.lsp") -replace "%USERPROFILE%", "$Env:userprofile" -replace "\\", "/" | Out-File "$localDestDir\load_dll.lsp"
     }
 } catch {
-    mkdir -Force -Path "$localDestDir\GMEPElectricalCommands"
-    Copy-Item -Path "$zDriveSourceDir\GMEPElectricalCommands\latest\*" -Destination "$localDestDir\GMEPElectricalCommands" -Recurse
     Copy-Item "$zDriveSourceDir\load_dll.lsp" -Destination "$localDestDir"
-    Get-ChildItem -Recurse "$localDestDir\GMEPElectricalCommands\" | Unblock-File
+    Get-Item "$localDestDir\load_dll.lsp" | Unblock-File
     (Get-Content "$localDestDir\load_dll.lsp") -replace "%USERPROFILE%", "$Env:userprofile" -replace "\\", "/" | Out-File "$localDestDir\load_dll.lsp"
 }
