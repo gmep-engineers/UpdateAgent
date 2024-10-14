5f7d71b9a81f66600dde6c21de87180540e8af6c
grenrock <stevengrenrock@live.com>
2024-10-14 09:56:09 -0700

    create shortcut

--- Installer.ps1
+++ Installer.ps1
@@ -3,3 +3,8 @@ $localDestDir = "$Env:userprofile\AutoCAD Commands"
 mkdir -Force -Path "$localDestDir"
 Copy-Item .\UpdateAgent.ps1 -Destination "$localDestDir"
 & $localDestDir\UpdateAgent.ps1
+$shell = New-Object -comObject WScript.Shell
+$shortcut = $shell.CreateShortcut("$Env:userprofile\Desktop\Update AutoCAD Commands.lnk")
+$shortcut.TargetPath = "powershell.exe"
+$shortcut.Arguments = "-ExecutionPolicy Bypass -File `"$localDestDir\UpdateAgent.ps1`""
+$shortcut.Save()
